8ff816c5e9cf0821486d2e17a8b5f0fa8c0fd470
ogane0112 <kai513272@gmail.com>
2024-05-14 02:27:13 +0900

    study 繰り返し2乗法 + 累積和

---
+++ mathBook/4/累積和.py
@@ -0,0 +1,10 @@
+n,q = map(int,input().split())
+a = list(map(int,input().split()))
+for i in range(n-1):
+    a[i+1] += a[i]
+a.insert(0,a[0])
+for i in range(q):
+    l,r = map(int,input().split())
+    print(a[r]-a[l])
+    
+print(a)
