b61bac9a7e0226cd43199c1c3b1b91b5fbf1f48f
ogane0112 <kai513272@gmail.com>
2024-05-17 11:43:54 +0900

    add bfs.pp

--- beginerContest/353/b.py
+++ beginerContest/353/b.py
@@ -5,9 +5,13 @@ num_starts = 0  # アトラクションの出発回数
 vacant_seats = K  # 空席数
 
 for group_size in A:
+
     if group_size < vacant_seats:  # グループの人数が空席数を超えている場合
+
         num_starts += 1  # アトラクションを出発させる
+
         vacant_seats = K  # 空席数をKに戻す
+        
     vacant_seats -= group_size  # 空席数を減らす
 
 print(num_starts)
